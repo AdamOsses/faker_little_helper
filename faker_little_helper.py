d47cfa14ca02ea296a7c91638d71214f014def9b
AdamOsses <adamosses@users.noreply.github.com>
2022-11-20 20:56:34 +0100

    data in brackets - sanitize filenames

--- faker_little_helper.py
+++ faker_little_helper.py
@@ -1,5 +1,6 @@
 import random
 from faker import Faker
+from pathvalidate import sanitize_filename
 
 fake = Faker()
 DATA = []
@@ -19,7 +20,8 @@ last_name_column_position = 0
 email_column_position = 0
 row_count = 0  # tmp
 selected_column = ''
-locale = []  # ['en', 'pl_PL']
+locale = ['en']  # ['en', 'pl_PL']
+files = []
 
 # Menu
 print('*' * 50)
@@ -35,7 +37,9 @@ providers = {
     'e': '{{email}}',
 }
 providers_char = ''.join(key for key in providers)
-selected_column_raw = "[cz_CZy 3 pl_PL]dadadabc"  # temp - add: input() - doubles and 'empty' char possible
+
+selected_column_raw = '["my_fi.csv le*1.csv" cz_CZy 1 pl_PLx "my_file.csv"]dadadabc'
+# temp - add: input() - doubles and 'empty' char possible
 
 # read data in brackets [] - row count; locales; filename
 bracket_l_pos = selected_column_raw.find('[')
@@ -45,6 +49,26 @@ if bracket_r_pos > bracket_l_pos:
     selected_column_raw = selected_column_raw[:bracket_l_pos] + selected_column_raw[bracket_r_pos+1:]
     print(f'-bracket_data: {bracket_data}')
     print(f'-selected_column_raw: {selected_column_raw}')
+    # file names: in ""
+    filename_pos = [i for i, c in enumerate(bracket_data) if c == '"']
+    print(f'filename pos: {filename_pos}')
+    if len(filename_pos) % 2 != 0:      # " should be even
+        print('Sth. wrong with " in []')
+    else:
+        # & check if proper .csv names
+        files_count = int(len(filename_pos)/2)
+        tmp_bracket_data = bracket_data[:]
+        for i in range(0, files_count+1, 2):
+            file = tmp_bracket_data[filename_pos[i]+1:filename_pos[i+1]]  # slice off file name from bracket data
+            print(file)
+            bracket_data = bracket_data.replace('"' + file + '"', '')  # remove "filename" from bracket data
+            if file[-4:] == '.csv': #  "sdfsdf.csv sgsgsg.csv"
+                # sanitize filename: https://pathvalidate.readthedocs.io/en/latest/pages/examples/sanitize.html
+                file = sanitize_filename(file)
+                files.append(file)
+        print(f"-{files}-->{bracket_data}<-")  # filename is between every two "
+
+    # locales(en cz_Cz ...) and row count
     elem = bracket_data.split(' ')
     print(elem)
     for e in elem:
@@ -59,7 +83,7 @@ if bracket_r_pos > bracket_l_pos:
                 locale.append(e)
 
 
-# remove doubles and 'empty' chars
+# remove doubles and unused chars
 for c in selected_column_raw:
     if c not in selected_column and c in providers_char:
         selected_column += c
@@ -67,6 +91,7 @@ for c in selected_column_raw:
 print(f'providers_char: {providers_char}\nselected_column_raw: {selected_column_raw}'
       f'\nselected_column: {selected_column}')
 
+# columns that affect each other
 if 'd' in selected_column:
     is_gender_column = True
     gender_column_position = selected_column.index('d')
@@ -89,7 +114,7 @@ print('raw data: ', DATA)
 
 # generate and write fake rows
 fake = Faker(locale)
-file = open('file.csv', 'w', newline='')
+file = open('file.csv', 'w', newline='')    # add list of files
 for _ in range(row_count):
     fake_last_name = fake.last_name()
     fake_first_name = fake.first_name()
