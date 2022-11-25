9c017011d06bc4397109195c6d4a732479c8d3c1
AdamOsses <adamosses@users.noreply.github.com>
2022-11-26 00:15:16 +0100

    add input data

--- faker_little_helper.py
+++ faker_little_helper.py
@@ -1,5 +1,6 @@
 import random
 import os
+import sys
 import readline
 from faker import Faker
 from pathvalidate import sanitize_filename
@@ -31,10 +32,7 @@ name_column_position = 0
 first_name_column_position = 0
 last_name_column_position = 0
 email_column_position = 0
-row_count = 0
 selected_column = ''
-locale = []  # ['en', 'pl_PL']
-files = []
 
 
 providers = {
@@ -72,61 +70,72 @@ print('Use [] to change default values: \n'
       '   [10 "file1.csv" "file2.csv"]mgah - write 10 data rows to file1.csv and file2.csv, columns: mgah')
 print('*' * 100)
 
-selected_column_raw = ''  # '["my_file*1.csv" cz_CZ 1 pl_PLx "my_file.csv"]ejklmn'
+selected_column_raw = '' # selected_column_raw ='["my_file*1.csv" cz_CZ 1 pl_PLx "my_file.csv"]ejklmn'
+text = ''
 # temp - add: input() - doubles and 'empty' char possible
 
 # read data in brackets [] - row count; locales (cz_CZ, en...); filename
-selected_column_raw = txt_input("Set data: ")
-bracket_l_pos = selected_column_raw.find('[')
-bracket_r_pos = selected_column_raw.find(']')
-if bracket_r_pos > bracket_l_pos:
-    bracket_data = selected_column_raw[bracket_l_pos+1:bracket_r_pos]
-    selected_column_raw = selected_column_raw[:bracket_l_pos] + selected_column_raw[bracket_r_pos+1:]
-    print(f'-bracket_data: {bracket_data}')
-    print(f'-selected_column_raw: {selected_column_raw}')
-    # file names: in ""
-    filename_pos = [i for i, c in enumerate(bracket_data) if c == '"']
-    print(f'filename pos: {filename_pos}')
-    if len(filename_pos) % 2 != 0:      # " should be even
-        print('Sth. wrong with " in []')
-    elif len(filename_pos) > 0:
-        # & check if proper .csv names
-        files_count = int(len(filename_pos)/2)
-        tmp_bracket_data = bracket_data[:]
-        for i in range(0, files_count+1, 2):
-            file = tmp_bracket_data[filename_pos[i]+1:filename_pos[i+1]]  # slice off file name from bracket data
-            print(file)
-            bracket_data = bracket_data.replace('"' + file + '"', '')  # remove "filename" from bracket data
-            if file[-4:] == '.csv': #  "sdfsdf.csv sgsgsg.csv"
-                # sanitize filename: https://pathvalidate.readthedocs.io/en/latest/pages/examples/sanitize.html
-                file = sanitize_filename(file)
-                files.append(file)
-        print(f"-{files}-->{bracket_data}<-")  # filename is between every two "
+while True:
+    text = txt_input("Input data: ", text)
+    if text[-1] == 'X':
+        sys.exit("Thank you for using faker little helper.")
+    elif text == selected_column_raw:
+        break
+    selected_column_raw = text
+    columns_data = text
+    row_count = 0
+    locale = []  # ['en', 'pl_PL']
+    files = []   # .csv
+    bracket_l_pos = selected_column_raw.find('[')
+    bracket_r_pos = selected_column_raw.find(']')
+    if bracket_r_pos > bracket_l_pos:
+        bracket_data = selected_column_raw[bracket_l_pos+1:bracket_r_pos]
+        columns_data = selected_column_raw[:bracket_l_pos] + selected_column_raw[bracket_r_pos+1:]
+        print(f'-bracket_data: {bracket_data}')
+        print(f'-selected_column_raw: {selected_column_raw}')
+        # file names: in ""
+        filename_pos = [i for i, c in enumerate(bracket_data) if c == '"']
+        print(f'filename pos: {filename_pos}')
+        if len(filename_pos) % 2 != 0:      # " should be even
+            print('Sth. wrong with " in []')
+        elif len(filename_pos) > 0:
+            # & check if proper .csv names
+            files_count = int(len(filename_pos)/2)
+            tmp_bracket_data = bracket_data[:]
+            for i in range(0, files_count+1, 2):
+                file = tmp_bracket_data[filename_pos[i]+1:filename_pos[i+1]]  # slice off file name from bracket data
+                print(file)
+                bracket_data = bracket_data.replace('"' + file + '"', '')  # remove "filename" from bracket data
+                if file[-4:] == '.csv': #  "sdfsdf.csv sgsgsg.csv"
+                    # sanitize filename: https://pathvalidate.readthedocs.io/en/latest/pages/examples/sanitize.html
+                    file = sanitize_filename(file)
+                    files.append(file)
+            print(f"-{files}-->{bracket_data}<-")  # filename is between every two "
 
-    # locales(en cz_Cz ...) and row count
-    elem = bracket_data.split(' ')
-    print(elem)
-    for e in elem:
-        if e.isdigit():
-            row_count += int(e)
-        else:
-            try:
-                Faker(e)
-            except AttributeError:
-                print(f'Locale issue: {e} - check: https://faker.readthedocs.io/en/master/locales.html')
+        # locales(en cz_Cz ...) and row count
+        elem = bracket_data.split(' ')
+        print(elem)
+        for e in elem:
+            if e.isdigit():
+                row_count += int(e)
             else:
-                locale.append(e)
+                try:
+                    Faker(e)
+                except AttributeError:
+                    print(f'Locale issue: {e} - check: https://faker.readthedocs.io/en/master/locales.html')
+                else:
+                    locale.append(e)
 if len(locale) == 0:
     locale.append('en')     # default locale is en
 if row_count == 0:
     row_count = 20
 
 # remove doubles and unused chars
-for c in selected_column_raw:
+for c in columns_data:
     if c not in selected_column and c in providers_char:
         selected_column += c
 
-print(f'providers_char: {providers_char}\nselected_column_raw: {selected_column_raw}'
+print(f'providers_char: {providers_char}\nselected_column_raw: {columns_data}'
       f'\nselected_column: {selected_column}')
 
 # columns that affect each other
