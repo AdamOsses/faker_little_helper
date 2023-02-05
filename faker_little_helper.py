eadff952aad512c13833d8fe64a2f891a567c6ec
AdamOsses <adamosses@users.noreply.github.com>
2023-02-05 21:53:53 +0100

    file read, mod ifs

--- faker_little_helper.py
+++ faker_little_helper.py
@@ -77,7 +77,7 @@ text = ''
 # read data in brackets [] - row count; locales (cz_CZ, en...); filename
 while True:
     text = txt_input("Input data: ", text)
-    if text[-1] == 'X':
+    if len(text) == 0 or text[-1] == 'X':
         sys.exit("Thank you for using faker little helper.")
     elif text == selected_column_raw:
         break
@@ -90,34 +90,39 @@ while True:
     bracket_r_pos = selected_column_raw.find(']')
     if bracket_r_pos > bracket_l_pos:
         bracket_data = selected_column_raw[bracket_l_pos+1:bracket_r_pos]
-        columns_data = selected_column_raw[:bracket_l_pos] + selected_column_raw[bracket_r_pos+1:]
-        print(f'-bracket_data: {bracket_data}')
+        columns_data = selected_column_raw[:bracket_l_pos+1] + selected_column_raw[bracket_r_pos:]
+        print(f'-bracket_data->{bracket_data}')
         print(f'-selected_column_raw: {selected_column_raw}')
+        print(f'-columns_data->{columns_data}')
         # file names: in ""
         filename_pos = [i for i, c in enumerate(bracket_data) if c == '"']
-        print(f'filename pos: {filename_pos}')
+        print(f'filename pos->{filename_pos}')
         if len(filename_pos) % 2 != 0:      # " should be even
             print('Sth. wrong with " in []')
         elif len(filename_pos) > 0:
             # & check if proper .csv names
             files_count = int(len(filename_pos)/2)
             tmp_bracket_data = bracket_data[:]
-            for i in range(0, files_count+1, 2):
+            for i in range(0, files_count*2, 2):
                 file = tmp_bracket_data[filename_pos[i]+1:filename_pos[i+1]]  # slice off file name from bracket data
-                print(file)
-                bracket_data = bracket_data.replace('"' + file + '"', '')  # remove "filename" from bracket data
+                print(f'file->{file}')
+                bracket_data = bracket_data.replace('"' + file + '"', "")  # remove "filename" from bracket data
+                print(f'bracket data after replace filename->{bracket_data}<-')
                 if file[-4:] == '.csv': #  "sdfsdf.csv sgsgsg.csv"
                     # sanitize filename: https://pathvalidate.readthedocs.io/en/latest/pages/examples/sanitize.html
                     file = sanitize_filename(file)
                     files.append(file)
-            print(f"-{files}-->{bracket_data}<-")  # filename is between every two "
+            print(f"-files->{files}<- bracket_data->{bracket_data}<-")  # filename is between every two "
 
         # locales(en cz_Cz ...) and row count
         elem = bracket_data.split(' ')
-        print(elem)
+        print(f'brackert_data->{bracket_data}<-')
+        print(f'elem->{elem}<-')
         for e in elem:
             if e.isdigit():
                 row_count += int(e)
+            elif len(e) < 2:
+                continue
             else:
                 try:
                     Faker(e)
@@ -125,10 +130,16 @@ while True:
                     print(f'Locale issue: {e} - check: https://faker.readthedocs.io/en/master/locales.html')
                 else:
                     locale.append(e)
+    print(f'*** File(s): {files if len(files)>0 else "file.csv"}')
+    print(f'*** Rows: {row_count if row_count>0 else "20"}')
+    print(f'*** Locale(s): {locale if len(locale)>0 else "en"}')
+
 if len(locale) == 0:
     locale.append('en')     # default locale is en
 if row_count == 0:
     row_count = 5
+if len(files) == 0:
+    files.append("file.csv")
 
 # remove doubles and unused chars
 for c in columns_data:
@@ -160,25 +171,21 @@ for column in selected_column:
 
 # generate and write fake rows
 fake = Faker(locale)
-file = open('file.csv', 'w', newline='')    # add list of files
+file = open(files[0], 'w', newline='')    # add list of files
 for _ in range(row_count):
+    # fake: name, last_name, first_name, gender and email are depend on each other:
     fake_last_name = fake.last_name()
     fake_first_name = fake.first_name()
     fake_name = fake_first_name + " " + fake_last_name
+    DATA[last_name_column_position] = fake_last_name
     if is_gender_column:
         fake_gender = random.choice(['male', 'female'])
         DATA[gender_column_position] = fake_gender
-    if (is_name_column or is_first_name_column) and is_gender_column:
         fake_first_name = fake.first_name_male() if fake_gender == 'male' else fake.first_name_female()
-        #  print(f'{fake_gender}-->{fake_first_name}')
         fake_name = fake_first_name + " " + fake_last_name
-        if is_first_name_column:
-            DATA[first_name_column_position] = fake_first_name
-    if is_name_column and (is_first_name_column or is_last_name_column or is_email_column or is_gender_column):
+    if is_name_column:
         DATA[name_column_position] = fake_name
-    if is_last_name_column and (is_name_column or is_email_column):
-        DATA[last_name_column_position] = fake_last_name
-    if is_first_name_column and (is_name_column or is_email_column):
+    if is_first_name_column:
         DATA[first_name_column_position] = fake_first_name
     if is_email_column:
         email_str = fake_first_name+'.'+fake_last_name+'_'+fake.domain_name()
