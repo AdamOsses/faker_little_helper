5d223ae285245a0adef4d02ea0a6812d83824ad9
AdamOsses <adamosses@users.noreply.github.com>
2023-02-05 23:21:03 +0100

    remove unused variables

--- faker_little_helper.py
+++ faker_little_helper.py
@@ -14,33 +14,20 @@ def txt_input(prompt, prefill=''):
         readline.set_startup_hook()
 
 
-os.system('clear')
-
 fake = Faker()
 DATA = []
 fake_gender = '-X-'
 fake_name = '-X-'
 fake_first_name = '-X-'
 fake_last_name = '-X-'
-is_gender_column = False
-is_name_column = False
-is_first_name_column = False
-is_last_name_column = False
-is_email_column = False
-gender_column_position = 0
-name_column_position = 0
-first_name_column_position = 0
-last_name_column_position = 0
-email_column_position = 0
 selected_column = ''
-
 
 providers = {
     'a': '{{name}}',
     'b': '{{first_name}}',
     'c': '{{last_name}}',
     'd': '--gender--',
-    'e': '{{email}}',
+    'e': '{{email}}',       # a-e can't touch this
     'f': '{{password}}',
     'g': '{{country}}',
     'h': '{{city}}',
@@ -56,6 +43,7 @@ providers = {
 providers_char = ''.join(key for key in providers)
 
 # Menu - prints all providers
+os.system('clear')
 print('*' * 100)
 for i in range(97, len(providers)+97):
     print(f'{chr(i)} - {providers[chr(i)][2:-2]}', end='\t')
@@ -149,23 +137,6 @@ for c in columns_data:
 #print(f'providers_char: {providers_char}\nselected_column_raw: {columns_data}'
 #      f'\nselected_column: {selected_column}')
 
-# columns that affect each other
-if 'd' in selected_column:
-    is_gender_column = True
-    gender_column_position = selected_column.index('d')
-if 'a' in selected_column:
-    is_name_column = True
-    name_column_position = selected_column.index('a')
-if 'b' in selected_column:
-    is_first_name_column = True
-    first_name_column_position = selected_column.index('b')
-if 'c' in selected_column:
-    is_last_name_column = True
-    last_name_column_position = selected_column.index('c')
-if 'e' in selected_column:
-    is_email_column = True
-    email_column_position = selected_column.index('e')
-
 for column in selected_column:
     DATA.append(providers.get(column, ''))
 
@@ -177,20 +148,20 @@ for _ in range(row_count):
     fake_last_name = fake.last_name()
     fake_first_name = fake.first_name()
     fake_name = fake_first_name + " " + fake_last_name
-    DATA[last_name_column_position] = fake_last_name
-    if is_gender_column:
+    DATA[selected_column.index('c')] = fake_last_name
+    if 'd' in selected_column:
         fake_gender = random.choice(['male', 'female'])
-        DATA[gender_column_position] = fake_gender
+        DATA[selected_column.index('d')] = fake_gender
         fake_first_name = fake.first_name_male() if fake_gender == 'male' else fake.first_name_female()
         fake_name = fake_first_name + " " + fake_last_name
-    if is_name_column:
-        DATA[name_column_position] = fake_name
-    if is_first_name_column:
-        DATA[first_name_column_position] = fake_first_name
-    if is_email_column:
+    if 'a' in selected_column:
+        DATA[selected_column.index('a')] = fake_name
+    if 'b' in selected_column:
+        DATA[selected_column.index('b')] = fake_first_name
+    if 'e' in selected_column:
         email_str = fake_first_name+'.'+fake_last_name+'_'+fake.domain_name()
         email_str = email_str.lower()
-        DATA[email_column_position] = email_str
+        DATA[selected_column.index('e')] = email_str
 
     row = fake.csv(data_columns=DATA, num_rows=1, include_row_ids=False)
     row = row.replace('_', '@')  # for some reason fake.csv() doesn't display '@' correctly
